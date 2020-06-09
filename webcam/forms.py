bc275a9166aa6d6e9422237adfde4c6570581320
Nadav <NadavK@users.noreply.github.com>
2020-06-09 23:21:41 +0300

    Using HTML webcam

--- webcam/forms.py
+++ webcam/forms.py
@@ -5,8 +5,10 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from webcam.widgets import CameraWidget
 
 
+#TODO: Consider deriving from ImageField
 class CameraField(forms.FileField):
     widget = CameraWidget
+
 
     def __init__(self, width=320, height=240, format='jpg',
                  camera_width=320, camera_height=240, *args, **kwargs):
@@ -15,22 +17,29 @@ class CameraField(forms.FileField):
         self.height = height
         self.camera_width = camera_width
         self.camera_height = camera_height
-        super(CameraField, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     def widget_attrs(self, widget):
-        return {'width': self.width,
+        attrs = super().widget_attrs(widget)
+        attrs.update({'width': self.width,
                 'height': self.height,
                 'format': self.format,
                 'camera_width': self.camera_width,
-                'camera_height': self.camera_height}
+                'camera_height': self.camera_height})
+        return attrs
 
-    def to_python(self, data):
-        filename, raw_val = data
-        if raw_val:
-            return SimpleUploadedFile(filename, base64.decodebytes(raw_val.encode()))
+    def Xto_python(self, data):
+        try:
+            filename, raw_val = data
+            if raw_val:
+                return SimpleUploadedFile(filename, base64.decodebytes(raw_val.encode()))
+        except:
+            return data
 
-    def clean(self, data, initial=None):
-        filename, raw_val = data
+    def Xclean(self, data, initial=None):
+        filename = data.name
+        raw_val = data.raw
+        #filename, raw_val = data
         if not filename:
             if initial:
                 filename = initial.name
--- webcam/widgets.py
+++ webcam/widgets.py
@@ -1,25 +1,46 @@
-from django.forms.widgets import Widget
+import base64
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.forms.widgets import Widget, ClearableFileInput
 from django.template.loader import render_to_string
+from datetime import datetime
 
 
-class CameraWidget(Widget):
+#class CameraWidget(Widget):
+class CameraWidget(ClearableFileInput):
     template = 'webcam/webcam.html'
 
-    def render(self, name, value, attrs=None):
-        defaults = {'name': name,
-                    'format': self.attrs['format'],
-                    'width': self.attrs['width'],
-                    'height': self.attrs['height'],
-                    'camera_width': self.attrs['camera_width'],
-                    'camera_height': self.attrs['camera_height'],
-                    'picture': value,
-                    'attrs': attrs}
-        defaults.update(attrs)
-        return render_to_string(self.template, defaults)
+    def render(self, name, value, attrs=None, renderer=None):
+        context = super().get_context(name, value, attrs)
+        context['widget'].update({
+            'name': name,
+            'format': self.attrs['format'],
+            'width': self.attrs['width'],
+            'height': self.attrs['height'],
+            'camera_width': self.attrs['camera_width'],
+            'camera_height': self.attrs['camera_height'],
+            #'picture': value,
+            'url': value and value.url,
+            'attrs': attrs,
+        })
+        #defaults.update(attrs)
+        return render_to_string(self.template, context)
 
     def value_from_datadict(self, data, files, name):
-        raw_val = data.get("data_%s" % name, None)
-        filename = data.get("%s" % name, None)
+        upload = super().value_from_datadict(data, files, name)
+        # proper INPUT FILE html upload either contains either the image, or "false" if the clear checkbox is selected
+        # will be "none" if upload is from camera-capture
+        if not upload is None:
+            return upload
+        raw_val = data.get(name, None)
         if raw_val:
             raw_val = raw_val.replace('data:image/jpeg;base64,', '')
-        return filename, raw_val
+            raw_val = raw_val.replace('data:image/png;base64,', '')
+        filename = "%s_%s" % (name, datetime.today().isoformat())       # NOTE: not threadsafe
+
+        return SimpleUploadedFile(filename, base64.decodebytes(raw_val.encode()))
+
+        data.name = filename
+        data.size = len(raw_val)
+        data.raw = raw_val
+        return data
+        #return filename, raw_val
